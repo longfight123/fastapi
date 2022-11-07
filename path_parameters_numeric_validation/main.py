1b81fd52274e613083fd43e19d6f32d2b5f7d9d4
Longfight123 <=>
2022-11-07 09:23:45 -0700

    gt lt le ge for numeric parameters

--- path_parameters_numeric_validation/main.py
+++ path_parameters_numeric_validation/main.py
@@ -15,11 +15,35 @@ app = FastAPI()
 #     return results
 
 # Parameters in any order that we need, even default values before parameters without default values
+# @app.get("/items/{item_id}")
+# async def read_items(
+#     *,
+#     item_id: int = Path(title="The ID of the item to get"),
+#     q: str | None
+# ):
+#     results = {"item_id": item_id}
+#     if q:
+#         results.update({"q": q})
+#     return results
+
+# Number validations in any parameter
+# @app.get("/items/{item_id}")
+# async def read_items(
+#     *,
+#     item_id: int = Path(title="The ID of the item to get", ge=1),
+#     q: str | None
+# ):
+#     results = {"item_id": item_id}
+#     if q:
+#         results.update({"q": q})
+#     return results
+
+# gt, lt, ge, le
 @app.get("/items/{item_id}")
 async def read_items(
     *,
-    item_id: int = Path(title="The ID of the item to get"),
-    q: str | None
+    item_id: int = Path(title="The ID of the item to get", ge=1, le=1000),
+    q: float | None = Query(gt=0, lt=1.5)
 ):
     results = {"item_id": item_id}
     if q:
