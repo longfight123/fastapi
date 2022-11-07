4f6c09a8bb85901b2565e4b5c6a572bce59e9a33
Longfight123 <=>
2022-11-07 08:52:48 -0700

    adding metadata to query parameters

--- query_parameters_string_validations/main.py
+++ query_parameters_string_validations/main.py
@@ -11,8 +11,29 @@ app = FastAPI()
 #     return results
 
 # Using query with regex
+# @app.get("/items")
+# async def read_items(q: str | None = Query(default=None, max_length=50, regex="^fixedquery$")):
+#     results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+#     if q:
+#         results.update({"q": q})
+#     return results
+
+# Using Query to require a list of values as a query parameter
 @app.get("/items")
-async def read_items(q: str | None = Query(default=None, max_length=50, regex="^fixedquery$")):
+async def read_items(q: list[str] | None = Query(default=None, max_length=3)):
+    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+    if q:
+        results.update({"q": q})
+    return results
+
+# Using Query and adding metadata to the query parameter
+@app.get("/items")
+async def read_items(q: list[str] | None = Query(
+    default=None, 
+    title="Query string", 
+    description="Query string for items to search in database", 
+    max_length=3
+)):
     results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
     if q:
         results.update({"q": q})
