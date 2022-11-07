2bdf1e5b877b337e74e9929847d1d82e4ab5ce92
Longfight123 <=>
2022-11-07 09:09:48 -0700

    Path parameters with metadata

---
+++ path_parameters_numeric_validation/main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI, Path, Query
+
+app = FastAPI()
+
+
+# Path parameter with meta data
+app.get("/items/{item_id}")
+async def read_items(
+    item_id: int = Path(title="The ID of the item to get"),
+    q: str | None = Query(default=None, alias="item-query")
+):
+    results = {"item_id": item_id}
+    if q:
+        results.update({"q": q})
+    return results
