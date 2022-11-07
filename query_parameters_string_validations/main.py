7a15fde9acbfbe0834108c0e7924a329996832e9
Longfight123 <=>
2022-11-07 09:03:28 -0700

    alias query parameter names

--- query_parameters_string_validations/main.py
+++ query_parameters_string_validations/main.py
@@ -19,20 +19,34 @@ app = FastAPI()
 #     return results
 
 # Using Query to require a list of values as a query parameter
-@app.get("/items")
-async def read_items(q: list[str] | None = Query(default=None, max_length=3)):
-    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
-    if q:
-        results.update({"q": q})
-    return results
+# @app.get("/items")
+# async def read_items(q: list[str] | None = Query(default=None, max_length=3)):
+#     results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+#     if q:
+#         results.update({"q": q})
+#     return results
 
 # Using Query and adding metadata to the query parameter
+# @app.get("/items")
+# async def read_items(q: list[str] | None = Query(
+#     default=None, 
+#     title="Query string", 
+#     description="Query string for items to search in database", 
+#     max_length=3
+# )):
+#     results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+#     if q:
+#         results.update({"q": q})
+#     return results
+
+# Alias parameter names
 @app.get("/items")
 async def read_items(q: list[str] | None = Query(
     default=None, 
     title="Query string", 
     description="Query string for items to search in database", 
-    max_length=3
+    max_length=3,
+    alias="item-query"
 )):
     results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
     if q:
