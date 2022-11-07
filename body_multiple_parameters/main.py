e0ab7978f3a6d7ebc686ed28bd16ff28685d62dc
Longfight123 <=>
2022-11-07 09:36:44 -0700

    Multiple body parameters

--- body_multiple_parameters/main.py
+++ body_multiple_parameters/main.py
@@ -9,17 +9,34 @@ class Item(BaseModel):
     price: float
     tax: float | None = None
 
+class User(BaseModel):
+    username: str
+    full_name: str | None = None
+
+# Declaring path, query, and body parameters
+# @app.put("/items/{item_id}")
+# async def update_item(
+#     *,
+#     item_id: int = Path(title="The ID of the item to get", ge=0, le=1000),
+#     q: str | None = None,
+#     item: Item | None = None
+# ):
+#     results = {"item_id": item_id}
+#     if q:
+#         results.update({"q": q})
+#     if item:
+#         results.update({"item": item})
+#     return results
+
 # Declaring path, query, and body parameters
 @app.put("/items/{item_id}")
 async def update_item(
     *,
     item_id: int = Path(title="The ID of the item to get", ge=0, le=1000),
-    q: str | None = None,
-    item: Item | None = None
+    user: User,
+    item: Item
 ):
     results = {"item_id": item_id}
-    if q:
-        results.update({"q": q})
-    if item:
-        results.update({"item": item})
+    results.update({"item": item})
+    results.update({"user": user})
     return results
