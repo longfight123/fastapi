0f20a7d466b708c02598c084fbfa936a05306ac5
Longfight123 <=>
2022-11-08 10:27:25 -0700

    deeply nested models

--- body_nested_models/main.py
+++ body_nested_models/main.py
@@ -54,11 +54,38 @@ app = FastAPI()
 
 
 # Declare a submodel Image
+# class Image(BaseModel):
+#     url: HttpUrl
+#     name: str
+
+# Declare a list of submodels
+# class Item(BaseModel):
+#     name: str
+#     description: str | None = Field(default=None, title="The description of the item", max_length=300)
+#     price: float
+#     tax: float | None = Field(default=None, description="The price must be greater than 0", gt=0)
+#     tags: set[str] = set()
+#     images: list[Image] | None = None
+
+# Declare a list field in the model
+# @app.put("/items/{item_id}")
+# async def update_item(
+#     *,
+#     item_id: int = Path(title="The ID of the item to get", ge=0, le=1000),
+#     item: Item
+# ):
+#     results = {"item_id": item_id}
+#     results.update({"item": item})
+#     return results
+
+
+
+# Deeply Nested Models
+
 class Image(BaseModel):
     url: HttpUrl
     name: str
 
-# Declare a list of submodels
 class Item(BaseModel):
     name: str
     description: str | None = Field(default=None, title="The description of the item", max_length=300)
@@ -67,13 +94,12 @@ class Item(BaseModel):
     tags: set[str] = set()
     images: list[Image] | None = None
 
-# Declare a list field in the model
-@app.put("/items/{item_id}")
-async def update_item(
-    *,
-    item_id: int = Path(title="The ID of the item to get", ge=0, le=1000),
-    item: Item
-):
-    results = {"item_id": item_id}
-    results.update({"item": item})
-    return results
+class Offer(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    items: list[Item]
+
+@app.put("/offers")
+async def update_item(*,  offer: Offer):
+    return offer
