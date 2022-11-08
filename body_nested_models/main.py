6a45b594df249ab38f37261f4836afce8597633b
Longfight123 <=>
2022-11-08 10:01:33 -0700

    Declare a set attribute in the model to only get unique items

--- body_nested_models/main.py
+++ body_nested_models/main.py
@@ -4,12 +4,20 @@ from pydantic import BaseModel, Field
 app = FastAPI()
 
 # Declare the type of list in Python 3.11
+# class Item(BaseModel):
+#     name: str
+#     description: str | None = Field(default=None, title="The description of the item", max_length=300)
+#     price: float
+#     tax: float | None = Field(default=None, description="The price must be greater than 0", gt=0)
+#     tags: list[str] = []
+    
+# Declare the type of set in Python 3.11
 class Item(BaseModel):
     name: str
     description: str | None = Field(default=None, title="The description of the item", max_length=300)
     price: float
     tax: float | None = Field(default=None, description="The price must be greater than 0", gt=0)
-    tags: list[str] = []
+    tags: set[str] = {}
 
 # Declare a list field in the model
 @app.put("/items/{item_id}")
