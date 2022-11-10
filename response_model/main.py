75fe60640b75661be9db4e9bba7ee4f0c60ddde5
Longfight123 <=>
2022-11-10 09:41:16 -0700

    Return the same input model

--- response_model/main.py
+++ response_model/main.py
@@ -1,17 +1,34 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
 
 app = FastAPI()
 
 # Using a response model for data validation purposes
 
-class Item(BaseModel):
-    name: str
-    description: str | None = None
-    price: float
-    tax: float | None = None
-    tags: list[str] = []
+# class Item(BaseModel):
+#     name: str
+#     description: str | None = None
+#     price: float
+#     tax: float | None = None
+#     tags: list[str] = []
 
-@app.post("/items", response_model=Item)
-async def create_item(item: Item):
-    return item
+# @app.post("/items", response_model=Item)
+# async def create_item(item: Item):
+#     return item
+
+
+
+# Returning the same input data
+
+# class UserIn(BaseModel):
+#     username: str
+#     password: str
+#     email: EmailStr
+#     full_name: str | None = None
+
+# @app.post("/user", response_model=UserIn)
+# async def create_user(user: UserIn):
+#     return user
+
+
+
