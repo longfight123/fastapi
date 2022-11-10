eb31ea681c0c8fa46c5019174d110c09bc8e65d6
Longfight123 <=>
2022-11-10 09:43:49 -0700

    return a different response model

--- response_model/main.py
+++ response_model/main.py
@@ -32,3 +32,19 @@ app = FastAPI()
 
 
 
+# Returning a different output model
+
+class UserIn(BaseModel):
+    username: str
+    password: str
+    email: EmailStr
+    full_name: str | None = None
+
+class UserOut(BaseModel):
+    username: str
+    email: EmailStr
+    full_name: str | None = None
+
+@app.post("/user", response_model=UserOut)
+async def create_user(user: UserIn):
+    return user
