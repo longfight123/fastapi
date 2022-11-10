14410a9dd3d8f03e19e35708b15e010a1f610090
Longfight123 <=>
2022-11-10 10:12:53 -0700

    response_model_include and response_model_exclude

--- response_model/main.py
+++ response_model/main.py
@@ -53,19 +53,45 @@ app = FastAPI()
 
 # Using response_model_exclude_unset=True parameter
 
+# class Item(BaseModel):
+#     name: str
+#     description: str | None = None
+#     price: float
+#     tax: float | None = None
+#     tags: list[str] = []
+
+# items = {
+#     "foo": {"name": "Foo", "price": 50.2},
+#     "bar": {"name": "Bar", "description": "The bartenders", "price": 62, "tax": 20.2},
+#     "baz": {"name": "Baz", "description": None, "price": 50.2, "tax": 10.5, "tags": []},
+# }
+
+# @app.get("/items/{item_id}", response_model=Item, response_model_exclude_unset=True)
+# async def read_items(item_id: str):
+#     return items[item_id]
+
+
+
+
+# response_model_include and response_model_exclude
+
+
 class Item(BaseModel):
     name: str
     description: str | None = None
     price: float
     tax: float | None = None
-    tags: list[str] = []
 
 items = {
     "foo": {"name": "Foo", "price": 50.2},
     "bar": {"name": "Bar", "description": "The bartenders", "price": 62, "tax": 20.2},
-    "baz": {"name": "Baz", "description": None, "price": 50.2, "tax": 10.5, "tags": []},
+    "baz": {"name": "Baz", "description": None, "price": 50.2, "tax": 10.5},
 }
 
-@app.get("/items/{item_id}", response_model=Item, response_model_exclude_unset=True)
+@app.get("/items/{item_id}/name", response_model=Item, response_model_include={"name"})
 async def read_items(item_id: str):
+    return items[item_id]
+
+@app.get("/items/{item_id}/public", response_model=Item, response_model_exclude={"tax"})
+async def read_item_public_date(item_id: str):
     return items[item_id]
