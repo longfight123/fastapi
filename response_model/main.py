5941e2ddb112d2f14559d9d59441b0c295244697
Longfight123 <=>
2022-11-10 10:07:23 -0700

    response_model_exclude_unset

--- response_model/main.py
+++ response_model/main.py
@@ -34,17 +34,38 @@ app = FastAPI()
 
 # Returning a different output model
 
-class UserIn(BaseModel):
-    username: str
-    password: str
-    email: EmailStr
-    full_name: str | None = None
+# class UserIn(BaseModel):
+#     username: str
+#     password: str
+#     email: EmailStr
+#     full_name: str | None = None
 
-class UserOut(BaseModel):
-    username: str
-    email: EmailStr
-    full_name: str | None = None
+# class UserOut(BaseModel):
+#     username: str
+#     email: EmailStr
+#     full_name: str | None = None
 
-@app.post("/user", response_model=UserOut)
-async def create_user(user: UserIn):
-    return user
+# @app.post("/user", response_model=UserOut)
+# async def create_user(user: UserIn):
+#     return user
+
+
+
+# Using response_model_exclude_unset=True parameter
+
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
+    tags: list[str] = []
+
+items = {
+    "foo": {"name": "Foo", "price": 50.2},
+    "bar": {"name": "Bar", "description": "The bartenders", "price": 62, "tax": 20.2},
+    "baz": {"name": "Baz", "description": None, "price": 50.2, "tax": 10.5, "tags": []},
+}
+
+@app.get("/items/{item_id}", response_model=Item, response_model_exclude_unset=True)
+async def read_items(item_id: str):
+    return items[item_id]
