b5d45bbd1685523e9951ec4f4a35a44caf33fc5e
Longfight123 <=>
2022-11-10 10:39:01 -0700

    creating a hierarchy of multiple models to reduce duplication

--- extra_models/main.py
+++ extra_models/main.py
@@ -3,24 +3,56 @@ from pydantic import BaseModel, EmailStr
 
 app = FastAPI()
 
+# Multiple models
+
+# class UserIn(BaseModel):
+#     name: str
+#     password: str
+#     email: EmailStr
+#     full_name: str | None = None
+
+# class UserOut(BaseModel):
+#     name: str
+#     email: EmailStr
+#     full_name: str | None = None
+
+# class UserDB(BaseModel):
+#     name: str
+#     email: EmailStr
+#     hashed_password: str
+#     full_name: str | None = None
+
+# def fake_password_hasher(user: UserIn):
+#     return "supersecret" + user.password
+
+# def fake_save_user(user: UserIn):
+#     hashed_password = fake_password_hasher(user)
+#     user_in_db = UserDB(**user.dict(), hashed_password=hashed_password)
+#     print("Saving " + user_in_db.name + " to database")
+#     return user_in_db
+
+# @app.post("/user", response_model=UserOut, response_model_exclude_unset=True)
+# async def create_user(user: UserIn):
+#     user_saved = fake_save_user(user)
+#     return user_saved
 
 
-class UserIn(BaseModel):
+
+# Hierarchy of multiple models
+
+class UserBase(BaseModel):
     name: str
+    email: EmailStr
+    full_name: str | None = None
+
+class UserIn(UserBase):
     password: str
-    email: EmailStr
-    full_name: str | None = None
 
-class UserOut(BaseModel):
-    name: str
-    email: EmailStr
-    full_name: str | None = None
+class UserOut(UserBase):
+    pass
 
-class UserDB(BaseModel):
-    name: str
-    email: EmailStr
+class UserDB(UserBase):
     hashed_password: str
-    full_name: str | None = None
 
 def fake_password_hasher(user: UserIn):
     return "supersecret" + user.password
