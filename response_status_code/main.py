285fce50a28f135269b2c1486e661178008b1436
Longfight123 <=>
2022-11-14 08:44:27 -0700

    Using the convenience variables in status instead of memorizing HTTP codes

--- response_status_code/main.py
+++ response_status_code/main.py
@@ -1,9 +1,19 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, status
 
 app = FastAPI()
 
 # Setting a status code for the response
 
 @app.post("/items/", status_code=201)
+async def read_items(name: str):
+    return {"name": name}
+
+
+
+
+
+# Using the convenience variables in fastapi.status instead of memorizing the codes
+
+@app.post("/items/", status_code=status.HTTP_201_CREATED)
 async def read_items(name: str):
     return {"name": name}
