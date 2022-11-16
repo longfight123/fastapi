9ddeed68db8279aaec0ecee243acf14e58a6a81c
Longfight123 <=>
2022-11-16 09:02:31 -0700

    Send multiple files using list[bytes]=File() or list[UploadFile]. Also use HTMLResponse to render a HTML file

--- request_files/main.py
+++ request_files/main.py
@@ -42,16 +42,46 @@ app = FastAPI()
 
 # Add metadata with UploadFile by using the File() class/function
 
-@app.post("/files")
-async def create_file(file: bytes | None = File(default=None, description="A file read as bytes")):
-    if file:
-        return {"file_size": len(file)}
-    else:
-        return {"file_size": "No file sent."}
+# @app.post("/files")
+# async def create_file(file: bytes | None = File(default=None, description="A file read as bytes")):
+#     if file:
+#         return {"file_size": len(file)}
+#     else:
+#         return {"file_size": "No file sent."}
 
-@app.post("/uploadfile")
-async def create_upload_file(file: UploadFile | None = File(default=None, description="A file read as UploadFile")):
-    if file:
-        return {"filename": file.filename}
-    else:
-        return {"filename": "No upload file sent."}
+# @app.post("/uploadfile")
+# async def create_upload_file(file: UploadFile | None = File(default=None, description="A file read as UploadFile")):
+#     if file:
+#         return {"filename": file.filename}
+#     else:
+#         return {"filename": "No upload file sent."}
+
+
+
+# Declaring multiple File Uploads
+
+from fastapi.responses import HTMLResponse
+
+@app.post("/files")
+async def create_file(files: list[bytes] = File()):
+    return {"file_sizes": [len(file) for file in files]}
+
+@app.post("/uploadfiles")
+async def create_upload_file(files: list[UploadFile]):
+    return {"filenames": [file.filename for file in files]}
+
+@app.get("/")
+async def main():
+    content = """
+    <body>
+    <form action="/files/" enctype="multipart/form-data" method="post">
+    <input name="files" type="file" multiple>
+    <input type="submit">
+    </form>
+    <form action="/uploadfiles/" enctype="multipart/form-data" method="post">
+    <input name="files" type="file" multiple>
+    <input type="submit">
+    </form>
+    </body>
+    """
+    return HTMLResponse(content=content)
