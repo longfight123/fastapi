7554783af5e1745500e4d6cbd89c8a7d3f935f76
Longfight123 <=>
2022-11-16 08:30:41 -0700

    Use file.read() if inside of an async function. Use file.file.read() if inside of a def function

--- request_files/main.py
+++ request_files/main.py
@@ -11,6 +11,10 @@ app = FastAPI()
 async def create_file(file: bytes = File()):
     return {"file_size": len(file)}
 
+# Use await file.read() if inside of a async function
+# Use file.file.read() if inside of a normal def function
+
 @app.post("/uploadfile")
 async def create_upload_file(file: UploadFile):
+    print(await file.read())
     return {"filename": file.filename}
