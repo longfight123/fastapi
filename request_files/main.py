aa0afb55692d18b48df1b0d82e53042c1afe1773
Longfight123 <=>
2022-11-16 08:40:04 -0700

    make a File or UploadFile optional using standard syntax. For File -> File(default=None) and for UploadFile UploadFile | None = None

--- request_files/main.py
+++ request_files/main.py
@@ -7,14 +7,32 @@ app = FastAPI()
 # If you declare the parameter type to be bytes, FastAPI will read the file for you and you will receive the
 # contents as bytes.
 
-@app.post("/files")
-async def create_file(file: bytes = File()):
-    return {"file_size": len(file)}
+# @app.post("/files")
+# async def create_file(file: bytes = File()):
+#     return {"file_size": len(file)}
 
-# Use await file.read() if inside of a async function
-# Use file.file.read() if inside of a normal def function
+# # Use await file.read() if inside of a async function
+# # Use file.file.read() if inside of a normal def function
+
+# @app.post("/uploadfile")
+# async def create_upload_file(file: UploadFile):
+#     print(await file.read())
+#     return {"filename": file.filename}
+
+
+
+# Make a file optional by using type annotation and default=None
+
+@app.post("/files")
+async def create_file(file: bytes | None = File(default=None)):
+    if file:
+        return {"file_size": len(file)}
+    else:
+        return {"file_size": "No file sent."}
 
 @app.post("/uploadfile")
-async def create_upload_file(file: UploadFile):
-    print(await file.read())
-    return {"filename": file.filename}
+async def create_upload_file(file: UploadFile | None = None):
+    if file:
+        return {"filename": file.filename}
+    else:
+        return {"filename": "No upload file sent."}
