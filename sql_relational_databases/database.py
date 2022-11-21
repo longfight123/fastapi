6e0b723b3c0c381c7a960c4886b2988c9134c7e6
Longfight123 <=>
2022-11-21 08:02:47 -0700

    setup database.py

---
+++ sql_relational_databases/database.py
@@ -0,0 +1,24 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+# Connection string
+SQLALCHEMY_DATABASE_URL = 'sqlite:///./sql_app.db'
+
+# Create SQLAlchemy engine, note the connect_args argument is only needed for SQLite
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL,
+    connect_args={"check_same_threads": False}
+)
+
+# Create SessionLocal class as our database session. 
+# Used to create instances of our database session
+# sessionamker is used to create the SessionLocal class
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine,
+)
+
+# declarative_base is used to create a Base class that our database models wil inherit from
+Base = declarative_base()
