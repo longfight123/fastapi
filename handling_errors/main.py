29bc0bf0a4407d5999c03d8dbe7cd66b06951b17
Longfight123 <=>
2022-11-28 08:09:57 -0700

    add custom headers in raised exception

--- handling_errors/main.py
+++ handling_errors/main.py
@@ -6,10 +6,23 @@ items = {
     "foo": "The Foo Wrestlers"
 }
 
+# @app.get("/items/{item_id}")
+# def get_item(item_id: str):
+#     if item_id in items:
+#         return {"item": items[item_id]}
+#     else:
+#         raise HTTPException(status_code=404, detail="Item not found")
+
+
+# Add a custom header
+
 @app.get("/items/{item_id}")
 def get_item(item_id: str):
     if item_id in items:
         return {"item": items[item_id]}
     else:
-        raise HTTPException(status_code=404, detail="Item not found")
-
+        raise HTTPException(
+            status_code=404, 
+            detail="Item not found",
+            headers={"x-custom-header": "custom value"}
+        )
