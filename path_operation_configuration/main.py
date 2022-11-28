ee7aca645a84c27ea293060f421ce33f312b4010
Longfight123 <=>
2022-11-28 09:20:34 -0700

    path operation summary and description

--- path_operation_configuration/main.py
+++ path_operation_configuration/main.py
@@ -34,18 +34,26 @@ class Item(BaseModel):
 
 # Tags with enum
 
-class Tags(Enum):
-    items = "items"
-    users = "users"
+# class Tags(Enum):
+#     items = "items"
+#     users = "users"
 
-@app.post("/items/", response_model=Item, tags=[Tags.items])
+# @app.post("/items/", response_model=Item, tags=[Tags.items])
+# async def create_item(item: Item):
+#     return item
+
+# @app.get("/items/", response_model=Item, tags=[Tags.items])
+# async def get_items():
+#     return [{"name": "Foo", "price": 42}]
+
+# @app.get("/users/", tags=[Tags.users])
+# async def get_users():
+#     return [{"username": "johndoe"}]
+
+
+# Summary and description
+
+@app.post("/items/", response_model=Item, summary="Create an item", description="Create an item with all the information, name, description, price, etc")
 async def create_item(item: Item):
     return item
 
-@app.get("/items/", response_model=Item, tags=[Tags.items])
-async def get_items():
-    return [{"name": "Foo", "price": 42}]
-
-@app.get("/users/", tags=[Tags.users])
-async def get_users():
-    return [{"username": "johndoe"}]
