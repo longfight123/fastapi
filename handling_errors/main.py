dc18ff3321f2376b9b9d651c67871449441a640f
Longfight123 <=>
2022-11-28 08:04:34 -0700

    raise a HTTPException

--- handling_errors/main.py
+++ handling_errors/main.py
@@ -1,2 +1,15 @@
 from fastapi import FastAPI, HTTPException
 
+app = FastAPI()
+
+items = {
+    "foo": "The Foo Wrestlers"
+}
+
+@app.get("/items/{item_id}")
+def get_item(item_id: str):
+    if item_id in items:
+        return {"item": items[item_id]}
+    else:
+        raise HTTPException(status_code=404, detail="Item not found")
+
