644e12980e80ab37fea75f0d768e08a83ce93eb6
Longfight123 <=>
2022-11-28 08:27:53 -0700

    install custom exception handlers

--- handling_errors/main.py
+++ handling_errors/main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Request
+from fastapi.responses import JSONResponse
 
 app = FastAPI()
 
@@ -16,13 +17,35 @@ items = {
 
 # Add a custom header
 
-@app.get("/items/{item_id}")
-def get_item(item_id: str):
-    if item_id in items:
-        return {"item": items[item_id]}
-    else:
-        raise HTTPException(
-            status_code=404, 
-            detail="Item not found",
-            headers={"x-custom-header": "custom value"}
-        )
+# @app.get("/items/{item_id}")
+# def get_item(item_id: str):
+#     if item_id in items:
+#         return {"item": items[item_id]}
+#     else:
+#         raise HTTPException(
+#             status_code=404, 
+#             detail="Item not found",
+#             headers={"x-custom-header": "custom value"}
+        # )
+
+# Raise custom exceptions
+
+class UnicornException(Exception):
+
+    def __init__(self, name: str):
+        self.name = name
+
+@app.exception_handler(UnicornException)
+def unicorn_exception_handler(request: Request, exception: UnicornException):
+    return JSONResponse(
+        status_code=418,
+        content={"message": f"Oops! {exception.name} did something."}
+    )
+
+@app.get("/unicorns/{name}")
+def read_unicorn(name: str):
+    if name == "yolo":
+        raise UnicornException(name=name)
+    return {"unicorn_name": name}
+
+
