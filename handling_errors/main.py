259f6532b2cb5d43db8002d9c6a9f8ba196ead20
Longfight123 <=>
2022-11-28 08:43:34 -0700

    override RequestValidationError and HTTPException exception handlers

--- handling_errors/main.py
+++ handling_errors/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, HTTPException, Request
-from fastapi.responses import JSONResponse
+from fastapi.responses import JSONResponse, PlainTextResponse
+from fastapi.exceptions import RequestValidationError
 
 app = FastAPI()
 
@@ -30,22 +31,43 @@ items = {
 
 # Raise custom exceptions
 
-class UnicornException(Exception):
+# class UnicornException(Exception):
 
-    def __init__(self, name: str):
-        self.name = name
+#     def __init__(self, name: str):
+#         self.name = name
 
-@app.exception_handler(UnicornException)
-def unicorn_exception_handler(request: Request, exception: UnicornException):
-    return JSONResponse(
-        status_code=418,
-        content={"message": f"Oops! {exception.name} did something."}
+# @app.exception_handler(UnicornException)
+# def unicorn_exception_handler(request: Request, exception: UnicornException):
+#     return JSONResponse(
+#         status_code=418,
+#         content={"message": f"Oops! {exception.name} did something."}
+#     )
+
+# @app.get("/unicorns/{name}")
+# def read_unicorn(name: str):
+#     if name == "yolo":
+#         raise UnicornException(name=name)
+#     return {"unicorn_name": name}
+
+
+# override request validation handlers and HTTPException handlers
+
+@app.exception_handler(RequestValidationError)
+def validation_exception_handler(request: Request, exception: RequestValidationError):
+    return PlainTextResponse(
+        status_code=422,
+        content=str(exception)
     )
 
-@app.get("/unicorns/{name}")
-def read_unicorn(name: str):
-    if name == "yolo":
-        raise UnicornException(name=name)
-    return {"unicorn_name": name}
+@app.exception_handler(HTTPException)
+def http_exception_handler(request: Request, exception: HTTPException):
+    return PlainTextResponse(
+        status_code=exception.status_code,
+        content=exception.detail
+    )
 
-
+@app.get("/items/{item_id}")
+def get_item(item_id: int):
+    if item_id == 3:
+        raise HTTPException(status_code=418, detail="Nope! I don't like 3.")
+    return {"item_id": item_id}
