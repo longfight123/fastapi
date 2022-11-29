16ca89cc8eff4e0b0f287a363684b3fe4af661ee
Longfight123 <=>
2022-11-29 08:08:15 -0700

    use the deprecated parameter in path operation decorator

--- path_operation_configuration/main.py
+++ path_operation_configuration/main.py
@@ -72,15 +72,27 @@ class Item(BaseModel):
 #     """
 #     return item
 
-@app.post("/items", response_model=Item, summary="Create an item", response_description="The created item")
-async def create_item(item: Item):
-    """
-    Create an item with all the information
+# @app.post("/items", response_model=Item, summary="Create an item", response_description="The created item")
+# async def create_item(item: Item):
+#     """
+#     Create an item with all the information
 
-    - **name**: each item must have a name
-    - **description**: a long description
-    - **price**: required
-    - **tax**: if the item doesn't have tax, omit this
-    - **tags**: a set of unique tag strings for this item
-    """
-    return item
+#     - **name**: each item must have a name
+#     - **description**: a long description
+#     - **price**: required
+#     - **tax**: if the item doesn't have tax, omit this
+#     - **tags**: a set of unique tag strings for this item
+#     """
+#     return item
+
+@app.get("/items", tags=["items"])
+async def get_items():
+    return [{"name": "Foo", "price": 42}]
+
+@app.get("/users", tags=["users"])
+async def get_users():
+    return [{"username": "johndoe"}]
+
+@app.get("item", tags=["items"], deprecated=True)
+async def get_item():
+    return {"name": "Foo", "price": 42}
