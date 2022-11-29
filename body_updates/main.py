310e607f8e92ff8b9019ea7815ee1d388473d0d1
Longfight123 <=>
2022-11-29 08:47:03 -0700

    update only parts of a row in a database with patch
    steps
    1) convert the item received to a dict using .dict(exclude_unset=True)
    2) convert the data in your database to pydantic model of the same type
    3) use the copy(update=update_item) method of the pydantic model of the already saved data, passing in the new data to update
    4) save the updated data to the database

--- body_updates/main.py
+++ body_updates/main.py
@@ -17,12 +17,29 @@ items = {
     "baz": {"name": "Baz", "description": None, "price": 50.2, "tax": 10.5, "tags": []},
 }
 
+# @app.get("/items/{item_id}", response_model=Item)
+# async def read_item(item_id: str):
+#     return items[item_id]
+
+# @app.put("/items/{item_id}", response_model=Item)
+# async def update_item(item_id: str, item: Item):
+#     items[item_id] = jsonable_encoder(item)
+#     print(items[item_id])
+#     return items[item_id]
+
+# Use patch and dict(exclude_unset) and pydantic models model.copy(update=update_model)
+# to copy over any attributes from update_model to model. Only copies the attributes
+# that were explicitly set in update_model and leaves the rest as is
+
 @app.get("/items/{item_id}", response_model=Item)
 async def read_item(item_id: str):
     return items[item_id]
 
-@app.put("/items/{item_id}", response_model=Item)
+@app.patch("/items/{item_id}", response_model=Item)
 async def update_item(item_id: str, item: Item):
-    items[item_id] = jsonable_encoder(item)
-    print(items[item_id])
-    return items[item_id]
+    update_item = item.dict(exclude_unset=True)
+    saved_item = items[item_id]
+    saved_item_model = Item(**saved_item)
+    saved_item_model = saved_item_model.copy(update=update_item)
+    items[item_id] = jsonable_encoder(saved_item_model)
+    return saved_item_model
